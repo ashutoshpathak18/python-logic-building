f17761b0cf1aa6264605e36cac0095283d4428a5
ashutoshpathak18 <pathakashutosh679@gmail.com>
2026-08-24 23:15:25 +0530

    Create 03_loops.py

---
+++ 01_Basics/03_loops.py
@@ -0,0 +1,46 @@
+# print numbers from 1 to n
+
+# n = int(input("Enter the value of n :"))
+# for i in range(1,n+1):
+#     print(f"{i}\n")
+
+
+# print all even numbers from 1 to 50
+
+# for i in range(1,51):
+#     if i%2 == 0:
+#         print(i)
+
+# Take a number n from the user and calculate the sum of numbers from 1 to n.
+
+# n = int(input("Enter the value of n: "))
+# sum = 0
+# for i in range(1,n+1):
+#     sum = sum+i
+# print(sum)
+
+
+# Take n and reduce it unit 1
+
+# n = int(input("Enter the value of n: "))
+# for i in range(0,n):
+#     print(n-i)
+
+# using while
+# while(n!=1):
+#     n=n-1
+#     print(n) 
+
+# Count digit of a number
+
+num = int(input("Enter a number :"))
+length = 0
+while num>0:
+    num = num//10
+    length+=1
+
+print(length)
+
+
+
+
